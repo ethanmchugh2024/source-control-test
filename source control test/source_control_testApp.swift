d210c3f6ee6c36160d5ae0d597f37c1ad6343efb
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-07-21 14:21:06 -0500

    Initial Commit

---
+++ source control test/source_control_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  source_control_testApp.swift
+//  source control test
+//
+//  Created by Ethan McHugh on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct source_control_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
